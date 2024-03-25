7edb46784f563c7b7b0ac102f2f03f3d0f2b8eda
matheuspereira00 <matheusinhofcb12@gmail.com>
2024-03-25 17:39:34 -0300

    branch

--- src/main/java/com/securityJwt/config/SecurityFilter.java
+++ src/main/java/com/securityJwt/config/SecurityFilter.java
@@ -11,7 +11,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import java.io.IOException;
 
 @Component
